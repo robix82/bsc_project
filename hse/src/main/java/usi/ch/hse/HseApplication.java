bb70d979b6b87cbee3fef8f23e7b71e949ae83d2
robix82 <robertjans82@outlook.com>
2020-10-12 14:26:50 +0200

    hse application skeleton

---
+++ hse/src/main/java/usi/ch/hse/HseApplication.java
@@ -0,0 +1,13 @@
+package usi.ch.hse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HseApplication.class, args);
+	}
+
+}
---
+++ hse/src/test/java/usi/ch/hse/HseApplicationTests.java
@@ -0,0 +1,13 @@
+package usi.ch.hse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
