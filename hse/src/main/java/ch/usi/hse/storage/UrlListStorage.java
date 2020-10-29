a38470cfb3203908c3e12847eb9a933d806ccd35
robix82 <robertjans82@outlook.com>
2020-10-29 17:01:21 +0100

    replaced TextFileStorage with UrlListStorage

---
+++ hse/src/main/java/ch/usi/hse/storage/UrlListStorage.java
@@ -0,0 +1,90 @@
+package ch.usi.hse.storage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import ch.usi.hse.exceptions.FileDeleteException;
+import ch.usi.hse.exceptions.FileReadException;
+import ch.usi.hse.exceptions.FileWriteException;
+import ch.usi.hse.exceptions.NoSuchFileException;
+
+/**
+ * utility class for storing and retrieving files
+ * in the url lists directory defined in application.properties
+ * 
+ * @author robert.jans@usi.ch
+ *
+ */
+@Component
+public class UrlListStorage extends FileStorage {
+
+	@Value("${url_lists_dir}")
+	private String storageDir;
+	
+	public void store(MultipartFile file) throws FileWriteException {
+		
+		Path path = Paths.get(storageDir);
+		store(file, path);
+	}
+	
+	public void delete(String fileName) throws NoSuchFileException, FileDeleteException {
+		
+		Path path = Paths.get(storageDir);
+		Path filePath = path.resolve(fileName);
+		
+		delete(filePath);
+	}
+	
+	public List<String> getLines(String fileName) throws NoSuchFileException, FileReadException {
+		
+		Path path = Paths.get(storageDir);
+		Path filePath = path.resolve(fileName);
+		
+		if (! Files.exists(filePath)) {
+			throw new NoSuchFileException(fileName);
+		}
+		
+		try {
+			return Files.readAllLines(filePath);
+		}
+		catch (IOException e) {
+			throw new FileReadException(fileName);
+		}
+	}
+	
+	public List<String> savedFiles() throws FileReadException {
+		
+		Path path = Paths.get(storageDir);
+		
+		try {
+			
+			List<String> files = Files.list(path).map(f -> f.getFileName().toString())
+											     .collect(Collectors.toList());
+			
+			return files;
+		}
+		catch (IOException e) {
+			throw new FileReadException(storageDir);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hse/src/test/java/ch/usi/hse/storage/UrlListStorageTest.java
@@ -0,0 +1,191 @@
+package ch.usi.hse.storage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+
+import ch.usi.hse.exceptions.FileDeleteException;
+import ch.usi.hse.exceptions.FileReadException;
+import ch.usi.hse.exceptions.FileWriteException;
+import ch.usi.hse.exceptions.NoSuchFileException;
+
+@SpringBootTest
+public class UrlListStorageTest {
+
+	@Value("${url_lists_dir}")
+	private String storageDir;
+	
+	@Autowired
+	private UrlListStorage listStorage;
+	
+	private Path storagePath;
+	
+	private List<String> savedList1, savedList2, newList, listNames;
+	private String newName;
+	
+	@BeforeEach
+	public void setUp() throws IOException {
+		
+		
+		storagePath = Paths.get(storageDir);
+		
+		String name1 = "list1.txt";
+		String name2 = "list2.txt";
+		listNames = Arrays.asList(name1, name2);
+		
+		savedList1 = Arrays.asList("url1", "url2", "url3");
+		savedList2 = Arrays.asList("url4", "url5", "url6");
+		newList = Arrays.asList("url7", "url8", "url9");
+		newName = "newList.txt";
+		
+		Files.list(storagePath).forEach(f -> {
+			try {
+				Files.deleteIfExists(f);
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+		
+		PrintStream ps1 = new PrintStream(new FileOutputStream(storageDir + name1));
+		
+		for (String s : savedList1) {
+			ps1.println(s);
+		}
+		
+		ps1.close();
+		
+		PrintStream ps2 = new PrintStream(new FileOutputStream(storageDir + name2));
+		
+		for (String s : savedList2) {
+			ps2.println(s);
+		}
+		
+		ps2.close();
+	}
+	
+	@Test
+	public void testSetup() throws IOException {
+		
+		assertEquals(2, Files.list(storagePath).count());
+	}
+	
+	@Test
+	public void testStore() throws IOException, FileWriteException {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (String s : newList) {
+			
+			sb.append(s).append("\n");
+		}
+		
+		MockMultipartFile file = new MockMultipartFile(newName,
+													   newName,
+													   MediaType.TEXT_PLAIN_VALUE,
+													   sb.toString().getBytes());
+		
+		assertEquals(2, Files.list(storagePath).count());
+		assertFalse(Files.exists(storagePath.resolve(newName)));
+		
+		listStorage.store(file);
+		
+		assertEquals(3, Files.list(storagePath).count());
+		assertTrue(Files.exists(storagePath.resolve(newName)));
+	}
+	
+	@Test
+	public void testDelete1() throws IOException, NoSuchFileException, FileDeleteException {
+		
+		String name = listNames.get(0);
+		
+		assertEquals(2, Files.list(storagePath).count());
+		assertTrue(Files.exists(storagePath.resolve(name)));
+		
+		listStorage.delete(name);
+		
+		assertEquals(1, Files.list(storagePath).count());
+		assertFalse(Files.exists(storagePath.resolve(name)));
+	}
+	
+	@Test
+	public void testDelete2() throws IOException, FileDeleteException {
+		
+		boolean exc = false;
+		
+		assertEquals(2, Files.list(storagePath).count());
+		
+		try {
+			listStorage.delete(newName);
+		}
+		catch (NoSuchFileException e) {
+			
+			assertTrue(e.getMessage().contains(newName));
+			exc = true;
+		}
+		
+		assertEquals(2, Files.list(storagePath).count());
+		assertTrue(exc);
+	}
+	
+	@Test
+	public void testGetLines1() throws NoSuchFileException, FileReadException {
+		
+		List<String> res = listStorage.getLines(listNames.get(0));
+
+		assertIterableEquals(savedList1, res);
+	}
+	
+	@Test
+	public void testGetLines2() throws FileReadException {
+		
+		boolean exc = false;
+		
+		try {
+			listStorage.getLines(newName);
+		}
+		catch (NoSuchFileException e) {
+			
+			assertTrue(e.getMessage().contains(newName));
+			exc = true;
+		}
+		
+		assertTrue(exc);
+	}
+	
+	@Test
+	public void testSavedFiles() throws FileReadException {
+		
+		List<String> fileNames = listStorage.savedFiles();
+		
+		assertEquals(2, fileNames.size());
+		assertTrue(fileNames.contains(listNames.get(0)));
+		assertTrue(fileNames.contains(listNames.get(1)));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
